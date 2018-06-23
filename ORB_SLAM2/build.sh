af1538913f8c3796ff351e21a46238a36d6ebc82
seonghunlee <sunghoon031@hotmail.com>
2018-06-23 18:30:53 +0200

    [ORB] use original cmake command in build.sh

--- ORB_SLAM2/build.sh
+++ ORB_SLAM2/build.sh
@@ -27,6 +27,5 @@ echo "Configuring and building ORB_SLAM2 ..."
 
 mkdir build
 cd build
-#cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
