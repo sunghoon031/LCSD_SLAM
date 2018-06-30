4b95333aebab33eda417fd39db37bc3b0be3a6ab
seonghunlee <sunghoon031@hotmail.com>
2018-06-30 21:47:33 +0200

    Trying to make ORB_SLAM ROS compile..

--- ORB_SLAM2/build_ros.sh
+++ ORB_SLAM2/build_ros.sh
@@ -3,6 +3,5 @@ echo "Building ROS nodes"
 cd Examples/ROS/ORB_SLAM2
 mkdir build
 cd build
-#cmake .. -DROS_BUILD_TYPE=Release
-cmake .. -G"Eclipse CDT4 - Unix Makefiles" -DROS_BUILD_TYPE=Release
+cmake .. -DROS_BUILD_TYPE=Release
 make -j
